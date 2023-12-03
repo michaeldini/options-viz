40d40a77884159d5764dea215e41cf4152fa158a
mdini <michaeldini@gmail.com>
2023-12-03 18:00:28 -0500

    edited the readme file

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # options-viz
-simple options view
+simple options visualization
+
+# Installation
+
+1. Download the repository by copying this code and run it in a terminal. A new folder will be created.
+```bash 
+pip clone https://github.com/michaeldini/options-viz.git 
+```
+
+2. Still in the terminal and inside the repository folder, copy this code and run it.
+```bash
+pip install -r requirements.txt
+```
+
+# Usage
+
+To use the program, navigate to the respoitory where the `options.py` file is. In the terminal run the command:
+```bash
+python options.py
+```
+or 
+```bash
+python3 options.py
+```
+A prompt should as what stock ticker you would like to research. Enter a valid ticker (lowercase or uppercase) and hit enter. The program should run and save a few image files in the current directory. Open the images from the file explorer. Researching the same stock a second time will overwrite the previous images. 
+
--- options.py
+++ options.py
@@ -6,44 +6,66 @@ import pandas as pd
 
 def gather_option_data(ticker: str):
 
+    # gather stock data
     stock = yf.Ticker(ticker)
-    expirys = stock.options
+    expiry_dates = stock.options
+    current_price = stock.info['currentPrice']
 
+    # use these lists to hold options data
+    # will concat the list into a df later
     volume_puts = []
     open_interest_puts = []
     volume_calls = []
     open_interest_calls = []
 
-    for expiry_date in expirys[:4]:
+    for expiry_date in expiry_dates[:5]:
+
+        # get option chain (puts & calls) for given date
         chain = stock.option_chain(expiry_date)
 
+        # get and append the puts data (df)
+        # label columns with expiry data
         puts = chain.puts.set_index('strike')
-        volume_puts.append(puts['volume'].rename('volume_' + expiry_date))
+        volume_puts.append(puts['volume'].rename(expiry_date))
         open_interest_puts.append(
-            puts['openInterest'].rename('openInterest_' + expiry_date))
+            puts['openInterest'].rename(expiry_date))
 
+        # get and append the puts data (df)
+        # label columns with expiry data
         calls = chain.calls.set_index('strike')
-        volume_calls.append(calls['volume'].rename('volume_' + expiry_date))
+        volume_calls.append(calls['volume'].rename(expiry_date))
         open_interest_calls.append(
-            calls['openInterest'].rename('openInterest_' + expiry_date))
+            calls['openInterest'].rename(expiry_date))
 
+    # concat each list. each df has index of strike price and columns for expiry dates
     data = {
-        'volume_puts': pd.concat(volume_puts, axis=1).sort_index(),
-        'open_interest_puts': pd.concat(open_interest_puts, axis=1).sort_index(),
-        'volume_calls': pd.concat(volume_calls, axis=1).sort_index(),
-        'open_interest_calls': pd.concat(open_interest_calls, axis=1).sort_index(),
+        'current_price': current_price,
+        'dfs': {
+            'volume_puts': pd.concat(volume_puts, axis=1).sort_index(),
+            'open_interest_puts': pd.concat(open_interest_puts, axis=1).sort_index(),
+            'volume_calls': pd.concat(volume_calls, axis=1).sort_index(),
+            'open_interest_calls': pd.concat(open_interest_calls, axis=1).sort_index(),
+        }
     }
+
     return data
 
 
 def visualize_options(ticker: str, data: dict):
-    for title, df in data.items():
+    for title, df in data['dfs'].items():
         df.replace(0, np.nan).plot(title=title, figsize=(
             14, 8))
         plt.savefig(f'{ticker}_{title}.png')
 
 
+def heatmap_options(ticker, data):
+    pass
+
+
 if __name__ == "__main__":
-    ticker = "MSFT".upper()
+    # ticker = "MSFT".upper()
+    ticker = input(
+        "An image file will be saved in the current directory for given ticker.\nEnter a ticker and hit enter: ").upper()
     data = gather_option_data(ticker)
     visualize_options(ticker, data)
+    heatmap_options(ticker, data)
